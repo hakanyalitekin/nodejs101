9d09c0b192f35b639a049b1b5ebc3582575f8183
Hakan Yalitekin <hakanyalitekin@gmail.com>
2022-03-15 02:00:29 +0300

    async edited.

--- 07_senkron_asenkron/app.mjs
+++ 07_senkron_asenkron/app.mjs
@@ -1,1 +1,12 @@
 //! Synchronous - Asynchronous
+//https://nexocode.com/blog/posts/behind-nodejs-event-loop/
+
+function makeFile(mahmut) {
+    console.log("Dosya oluştur.")
+
+    mahmut()
+}
+
+makeFile(function () {
+    console.log("Dosya oluşturulmaya başlandı.")
+})
